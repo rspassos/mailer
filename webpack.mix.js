78472db710fec2a82777f8308c1435401b981239
rspassos <rspassos@gmail.com>
2021-08-19 23:12:20 -0300

    cypress workflow (#1)
    * cypress workflow

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+require('laravel-mix-artisan-serve');
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ]).serve();
